a9559b35ba7bf4a2f8d0bb06587fc81370c2fc23
MeinChef <126389277+MeinChef@users.noreply.github.com>
2025-03-26 17:57:47 +0100

    added learning rule, draft

--- src/grad.py
+++ src/grad.py
@@ -1,6 +1,87 @@
 from imports import torch
+from torch.autograd.function import once_differentiable
+from imports import Callable
 
 # this function is not allowed to take any other hyperparmeters.
 # stuff like slope has to be defined within the function!
-def super_spike_21(input_, grad_input, spikes) -> torch.Tensor:
-    raise NotImplementedError()
+def super_spike_19(slope:float, alpha:float, beta:float) -> Callable:
+    """Sigmoid surrogate gradient enclosed with a parameterized parameters."""
+    slope = slope
+    alpha = alpha
+    beta = beta
+    
+    def inner(x):
+        return SuperSpike19.apply(x, alpha = alpha, beta = beta, slope = slope)
+    
+    return inner
+
+class SuperSpike19(torch.autograd.Function):
+    
+    """
+    Surrogate gradient of the Heaviside step function.
+
+    **Forward pass:** Heaviside step function shifted.
+
+        .. math::
+
+            S=\\begin{cases} 1 & \\text{if U ≥ U$_{\\rm thr}$} \\\\
+            0 & \\text{if U < U$_{\\rm thr}$}
+            \\end{cases}
+
+    **Backward pass:** Gradient of sigmoid function.
+
+        .. math::
+
+                TODO
+
+
+    Adapted from:
+
+    *F. Zenke, H. Mostafa, E. Neftci (2019) Surrogate Gradient Learning in Spiking Neural Networks
+    https://doi.org/10.48550/arXiv.1901.09948*"""
+
+    @staticmethod
+    def forward(ctx, input_: torch.Tensor, alpha: float, beta: float, slope: float) -> torch.Tensor:
+        ctx.save_for_backward(input_)
+        
+        ctx.slope = slope
+        ctx.alpha = alpha # synampic trace decay
+        ctx.beta = beta # membrane potential decay (I think we don't need that)
+
+        # Initialize dU_dW and dI_dW at the first step
+        if not hasattr(ctx, 'dU_dW'):
+            ctx.dU_dW = torch.zeros_like(input)
+            ctx.dI_dW = torch.zeros_like(input)
+
+
+        out = (input_ > 0).float()
+        return out
+    
+
+    @staticmethod
+    @once_differentiable
+    def backward(ctx, grad_outputs: torch.Tensor):
+        # input refers to the pre-trace
+        input_, = ctx.saved_tensors
+        grad_input = grad_outputs.clone()
+    
+        # compute the gradient (derivative of sigmoid)
+        grad = (
+            grad_input
+            * ctx.slope
+            * torch.exp(-ctx.slope * input_)
+            / ((torch.exp(-ctx.slope * input_) + 1) ** 2)
+        )
+
+        # Update the gradient terms (without negative term)
+        dI_dW = ctx.alpha * ctx.dI_dW + ctx.presynaptic_spike  # ∂I/∂W
+        dU_dW = ctx.beta * ctx.dU_dW + dI_dW  # ∂U/∂W
+
+        # Compute final weight update
+        weight_update = grad * dU_dW  
+
+        # Store updated gradients for the next step
+        ctx.dU_dW = dU_dW
+        ctx.dI_dW = dI_dW
+
+        return weight_update
