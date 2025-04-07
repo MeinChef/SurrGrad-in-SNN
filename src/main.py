1928c515e2aa98953ad040ff11069cb1da0bba72
MeinChef <126389277+MeinChef@users.noreply.github.com>
2025-04-07 19:05:14 +0200

    improvements

--- src/main.py
+++ src/main.py
@@ -26,12 +26,23 @@ def main() -> None:
 
     # training
     for epoch in range(config_model["epochs"]):
-        model.train_loop(data = train)
+        loss, acc, rec = model.train_loop(data = train)
+        
+        misc.stats_to_file(
+            config_data,
+            loss,
+            acc
+        )
         # model spikes in Memory usage after Training (~486 Batches a 128)
         model.test_loop(data = test)
+
+
+    
 
 if __name__ == "__main__":
     # comment next line out, if you have given your directory a custom name 
     misc.check_working_directory()
     
     main()
+
+    
