dddacfe11a8ef64f47095fa5c1d8911c7e4de1a8
MeinChef <126389277+MeinChef@users.noreply.github.com>
2025-04-07 16:35:47 +0200

    improved memory footprint of dataloding

--- src/data.py
+++ src/data.py
@@ -3,6 +3,9 @@ from imports import torch
 from imports import tonic
 from imports import torchvision
 
+###############################
+### OR SOME DATALOADER LEAK ###
+###############################
 
 # load the config.yml
 def load_config(path: str = "config.yml") -> tuple[dict, dict]:
@@ -17,9 +20,24 @@ def data_prep(config: dict) -> tuple[torch.utils.data.DataLoader, torch.utils.da
 
 
     sensor = tonic.datasets.NMNIST.sensor_size
-    
+
     # accumulate events to discrete "frames"
-    transform_frame = tonic.transforms.Compose(
+    transform_train = tonic.transforms.Compose(
+        [
+            tonic.transforms.Denoise(
+                filter_time = config["filter_time"]
+            ),
+
+            tonic.transforms.ToFrame(
+                sensor_size = sensor,
+                time_window = config["time_window"]
+            ),
+            torch.from_numpy,
+            torchvision.transforms.RandomRotation([-10,10])
+        ]
+    )
+    
+    transform_test = tonic.transforms.Compose(
         [
             tonic.transforms.Denoise(
                 filter_time = config["filter_time"]
@@ -29,53 +47,61 @@ def data_prep(config: dict) -> tuple[torch.utils.data.DataLoader, torch.utils.da
                 sensor_size = sensor,
                 time_window = config["time_window"]
             )
+            # without random rotation for testset
         ]
     )
+    
 
     # apply the transform to the datasets
     trainset = tonic.datasets.NMNIST(
         save_to = config["data_path"],
-        transform = transform_frame,
+        transform = transform_train,
         train = True    
     )
     testset = tonic.datasets.NMNIST(
         save_to = config["data_path"],
-        transform = transform_frame,
+        transform = transform_test,
         train = False
     )
 
-    # augment the trainset with rotations of the "frames"
-    transform = tonic.transforms.Compose(
-        [
-            torch.from_numpy,
-            torchvision.transforms.RandomRotation([-10,10])
-        ]
+
+
+    # cached_trainset = tonic.MemoryCachedDataset(trainset, transform = transform)
+    # cached_testset  = tonic.MemoryCachedDataset(testset)
+    
+    cached_trainset = tonic.DiskCachedDataset(
+        dataset = trainset, 
+        cache_path = config["cache_path"],
+    )
+    
+    cached_testset = tonic.DiskCachedDataset(
+        dataset = testset,
+        cache_path = config["cache_path"]
     )
 
-    # cache the datasets into Memory (requires 2.3GB of RAM)
-    cached_trainset = tonic.MemoryCachedDataset(trainset, transform = transform)
-    cached_testset  = tonic.MemoryCachedDataset(testset)
 
-
-    if config["DEBUG"]:
-        print("Sensor:", sensor)
-        print("Rough Size of Dataset in Memory:", len(pickle.dumps(cached_trainset)) + len(pickle.dumps(cached_testset)))
+    # if config["DEBUG"]:
+    #     print("Sensor:", sensor)
+    #     print("Rough Size of Dataset in Memory:", len(pickle.dumps(cached_trainset)) + len(pickle.dumps(cached_testset)))
         
     # prepare them for training, 
     trainloader = torch.utils.data.DataLoader(
-        cached_trainset, 
+        dataset = cached_trainset, 
         batch_size = config["batch_size"], 
         collate_fn = tonic.collation.PadTensors(batch_first = False), 
         shuffle = True,
         num_workers = config["worker"],
-        prefetch_factor = config["prefetch"]
+        prefetch_factor = config["prefetch"],
+        pin_memory = True
     )
+    
     testloader = torch.utils.data.DataLoader(
-        cached_testset, 
+        dataset = cached_testset, 
         batch_size = config["batch_size"], 
         collate_fn = tonic.collation.PadTensors(batch_first = False),
         num_workers = config["worker"],
-        prefetch_factor = config["prefetch"]
+        prefetch_factor = config["prefetch"],
+        pin_memory = True
     )
 
     return trainloader, testloader
