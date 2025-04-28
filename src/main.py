7a0f8620d9ec8b3d02431af80a18f9fdaa005d97
MeinChef <126389277+MeinChef@users.noreply.github.com>
2025-04-28 12:31:10 +0200

    added latency encoding setup

--- src/main.py
+++ src/main.py
@@ -12,9 +12,21 @@ def main() -> None:
 
     model = Model(config = config_model)
 
+    # setter for rate encoding
     # setter functions
-    model.set_loss(functional.loss.mse_count_loss(correct_rate = 0.8, incorrect_rate = 0.2))
-    model.set_acc(functional.acc.accuracy_rate)
+    # model.set_loss(functional.loss.mse_count_loss(correct_rate = 0.8, incorrect_rate = 0.2))
+    # model.set_acc(functional.acc.accuracy_rate)
+    # model.set_optim(
+    #     torch.optim.Adam(
+    #         model.parameters(), 
+    #         lr = config_model["learning_rate"], 
+    #         betas = (0.9, 0.99)
+    #     )
+    # )
+
+    # setter for latency encoding
+    model.set_loss(functional.loss.ce_temporal_loss())
+    model.set_acc(functional.acc.accuracy_temporal)
     model.set_optim(
         torch.optim.Adam(
             model.parameters(), 
