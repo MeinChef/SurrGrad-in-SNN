6e0a8afa99cfe3642ebe3b00733b5ec9db86a3f1
MeinChef <126389277+MeinChef@users.noreply.github.com>
2025-08-06 16:36:43 +0200

    made compliant with other changes

--- src/main.py
+++ src/main.py
@@ -6,35 +6,45 @@ from model import Model
 def main() -> None:
     config_data, config_model = data.load_config()
     train, test, num_classes = data.data_prep(config_data)
-    
+    handler = data.DataHandler(
+        config_data,
+        )
+
     if config_data["DEBUG"]:
         exit(0)
 
     config_model["num_classes"] = num_classes
     model = Model(config = config_model)
 
+
+    loss_hist_train = []
+    loss_hist_test = []
     # training
     for epoch in range(config_model["epochs"]):
         
-        loss, acc, rec = model.train_loop(data = train)
-        misc.stats_to_file(
-            config_data,
+        loss, acc, rec = model.fit(data = train)
+        handler.flush_to_file(
             loss,
             acc,
         )
+        loss_hist_train.append(loss)
 
-        loss, acc, rec = model.test_loop(data = test)
-        misc.stats_to_file(
-            config_data,
+        loss, acc, rec = model.evaluate(data = test)
+        handler.flush_to_file(
             loss,
             acc,
             rec,
             identifier = f"test-ep{epoch}"
         )
+        loss_hist_test.append(loss)
 
         # reset counter needed for recording hidden layers
         model.reset()
 
+    handler.plot_spikes(
+        identifier = "test",
+        infer_epochs = True
+    )
     # misc.plot_loss_acc(config_data)
     # misc.cleanup(config = config_data)
     
