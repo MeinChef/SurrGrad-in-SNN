3881d3eecb4c5b74d0270e94134f68b3b7032c06
MeinChef <126389277+MeinChef@users.noreply.github.com>
2025-08-12 18:47:06 +0200

    refined and re-structured code
    also run test in how well each implementation works

--- src/synth_data.py
+++ src/synth_data.py
@@ -1,60 +1,180 @@
 import torch
+import numpy as np
+import timeit
 
-def generate_spikes(
-    isi: torch.Tensor,
-    inputs: int,
-    r: int = 13,
-    T: int = 1000,
-    n_samples: int = 1000,
-) -> tuple[torch.Tensor, torch.Tensor]:
-    """
-    Function to generate Spikes, adapted from paper https://arxiv.org/abs/2507.16043v1
-    isi has to be of shape [classes] and be an integer tensor that specifies the inter-spike-intervals for each class
-    """
-    
-    no_spk_pairs = r * (T/1000)
-    n_classes = len(isi)
-    output = torch.zeros(
-        size = (T, n_samples, inputs),
-        dtype = torch.float32
-    )
-
-    samples_per_class_val = int(n_samples/ n_classes)
-    samples_per_class = torch.full_like(
-        input = isi,
-        fill_value = samples_per_class_val,
-        dtype = torch.int32
-    )
-    # make sure that we have n_samples in the array. Increase the last class with the remaining values
-    # dirty solution, but works
-    samples_per_class[-1] += n_samples - (n_classes * samples_per_class_val)
+# TODO: subclass generator for realsies, or return a torch dataloader somewhere
+class DataGenerator():
+    def __init__(
+            self,
+            time_steps: int,
+            inter_spike_intervals: np.ndarray,
+            rate: float = 15.,
+            neurons: int = 10,
+            no_samples: int = 3e4,
+            move_spikes: float = 0.,
+        ) -> None:
+        
+        """
+        isi should be 1d array, and len = classes
+        """
 
 
-    for i, sample in enumerate(samples_per_class):
-        time_stamps = torch.randint(
-            low = 0,
-            high = T - isi[i],
-            size = (sample, no_spk_pairs)
+        if rate < 5 or rate > 20:
+            raise ValueError(f"Rate is in an invalid range. Valid range is [5;20]. Actual value: {rate}")
+        if move_spikes < 0 or move_spikes > 1:
+            raise ValueError(f"Move_spikes is in an invalid range. Valid range is [0;1]. Actual value: {move_spikes}")
+
+        self.rng = np.random.default_rng()
+
+        self.time_steps = time_steps
+        self.neurons = neurons
+        self.inter_spike_intervals = inter_spike_intervals
+        self.no_spike_pairs_per_neuron = np.floor(rate * (self.time_steps / 1000))
+        self.total_samples = no_samples
+
+
+    def gen_data(
+        self,
+        isi: int = 10,
+    ) -> np.ndarray:
+        """
+        generate one sample 
+        """
+        if isi < 1 or isi > 10:
+            raise ValueError(f"ISI is in an invalid range. Valid range is [1;10]. Actual value: {isi}")
+        
+        full_data = []
+        for i in range(self.neurons):
+            valid_starts = np.arange(
+                self.time_steps - isi, 
+                dtype = np.int32, 
+            )
+
+            cur_no = 0
+            while cur_no < self.no_spike_pairs_per_neuron:
+                sample = self.rng.choice(
+                    valid_starts,
+                    size = (1,),
+                    replace = True,
+                    shuffle = False # not needed, but makes it faster
+                )[0]
+                full_data.extend(((sample, i), (sample + isi, i)))
+                valid_starts = valid_starts[abs(valid_starts - sample) > isi]
+                cur_no += 1
+        
+        spikes = np.array(full_data)
+        out = np.zeros(
+            shape = (self.time_steps, self.neurons),
+            dtype = np.float32,
         )
-        time_stamps = torch.sort(time_stamps)
+        out[spikes[:,0], spikes[:,1]] = 1
+        return out
+    
+    def gen_data_torch_cpu(
+        self,
+        isi: int = 10
+    ) -> torch.Tensor:
+        """
+        generate one sample 
+        """
+        if isi < 1 or isi > 10:
+            raise ValueError(f"ISI is in an invalid range. Valid range is [1;10]. Actual value: {isi}")
+        
+        full_data = []
+        for i in range(self.neurons):
+            valid_starts = torch.arange(
+                self.time_steps - isi, 
+                dtype = torch.int32,
+            )
 
-        bad_step_idx = []
-        for j, step in enumerate(time_stamps):
-            if j == 0: 
-                continue
-            if step - isi[i] <= time_stamps[j-1]:
-                bad_step_idx.append(j)
-            if step != time_stamps[-1]:
-                if step + isi[i] > time_stamps[j+1] and time_stamps[j-1] not in bad_step_idx:
+            cur_no = 0
+            while cur_no < self.no_spike_pairs_per_neuron:
+                sample = torch.randint(
+                    low = 0,
+                    high = len(valid_starts),
+                    size = (1,),
+                ).item()
+                sample = valid_starts[sample]
+                full_data.extend(((sample, i), (sample + isi, i)))
+                valid_starts = valid_starts[abs(valid_starts - sample) > isi]
+                cur_no += 1
 
-        for step in bad_step_idx:
-            # generate new step
-            # substract step from the whole array, take min of absolute value, that's the closest index i think
-            # if great, replace in time_stamps, sort time_stamps and repeat
-            pass
-        # do all of that at once and in-place
-        # [3,8,10,14]
-        # [-3, -1]   
+        spikes = torch.tensor(full_data)
+        out = torch.zeros(
+            size = (self.time_steps, self.neurons),
+            dtype = torch.float32,
+        )
+        out[spikes[:,0], spikes[:,1]] = 1
+        return out
+
+    def gen_data_torch_cuda(
+        self,
+        isi: int = 10
+    ) -> torch.Tensor:
+        """
+        generate one sample 
+        """
+        if isi < 1 or isi > 10:
+            raise ValueError(f"ISI is in an invalid range. Valid range is [1;10]. Actual value: {isi}")
+        
+        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
+
+        full_data = []
+        for i in range(self.neurons):
+            valid_starts = torch.arange(
+                self.time_steps - isi, 
+                dtype = torch.int32,
+                device = device
+            )
+
+            cur_no = 0
+            while cur_no < self.no_spike_pairs_per_neuron:
+                sample = torch.randint(
+                    low = 0,
+                    high = len(valid_starts),
+                    size = (1,),
+                    device = device
+                ).item()
+                sample = valid_starts[sample]
+                full_data.extend(((sample, i), (sample + isi, i)))
+                valid_starts = valid_starts[abs(valid_starts - sample) > isi]
+                cur_no += 1
+        
+        spikes = torch.tensor(full_data)
+        out = torch.zeros(
+            size = (self.time_steps, self.neurons),
+            dtype = torch.float32,
+            device = device
+        )
+        out[spikes[:,0], spikes[:,1]] = 1
+        return out
 
 
-    target = torch.stack([torch.full(size, cls) for cls, size in enumerate(samples_per_class)])
+if __name__ == "__main__":
+
+    data = DataGenerator(
+        time_steps = 1000,
+        inter_spike_intervals = np.arange(1)
+    )
+
+
+    print(timeit.timeit(
+        data.gen_data,
+        number = 10000
+    ))
+    # 22.460367957000017
+    # 22.72771648700018
+    # 22.611762075000115
+
+    print(timeit.timeit(
+        data.gen_data_torch_cpu,
+        number = 10000
+    ))
+    # 48.99222566199933
+    # 48.467646289000186
+
+    print(timeit.timeit(
+        data.gen_data_torch_cuda,
+        number = 10000
+    ))
+    # 157.93103715899997
