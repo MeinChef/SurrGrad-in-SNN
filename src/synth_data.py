d326c8941c3e2659be19cce0375a91dd937cfd02
MeinChef <126389277+MeinChef@users.noreply.github.com>
2025-09-15 15:55:06 +0200

    Re-Worked Data Generator
    Since I understood the datageneration wrong, some re-implementing had to be done. _generate_sample has been optimized. Class labels could be generated now more accurately, as they depend on both the ISI and the RATE.

--- src/synth_data.py
+++ src/synth_data.py
@@ -1,182 +1,192 @@
 import torch
 import numpy as np
 import timeit
+import math
 
 # https://stackoverflow.com/questions/14446128/append-vs-extend-efficiency
 
-# TODO: subclass generator for realsies, or return a torch dataloader somewhere
-class DataGenerator():
+class NewGenerator:
     def __init__(
-            self,
-            time_steps: int,
-            inter_spike_intervals: np.ndarray,
-            rate: float = 15.,
-            neurons: int = 10,
-            no_samples: int = 3e3,
-            move_spikes: float = 0.,
-        ) -> None:
-        
-        """
-        isi should be 1d array, and len = classes
-        """
+        self,
+        time_steps: int,
+        jitter: float = 0.0,
+        neurons: int = 10,
+        min_isi: int = 1,
+        max_isi: int = 10,
+        min_rate: int = 5,
+        max_rate: int = 20,
+    ):
+        self.time_steps = time_steps
+        self.jitter = jitter
+        self.neurons = neurons
+        self.min_isi  = min_isi
+        self.max_isi  = max_isi
+        self.min_rate = min_rate
+        self.max_rate = max_rate
 
-
-        if rate < 5 or rate > 20:
-            raise ValueError(f"Rate is in an invalid range. Valid range is [5;20]. Actual value: {rate}")
-        if move_spikes < 0 or move_spikes > 1:
-            raise ValueError(f"Move_spikes is in an invalid range. Valid range is [0;1]. Actual value: {move_spikes}")
-        if time_steps > np.iinfo(np.uint16).max:
-            raise ValueError(f"Maximum amount of time steps exceeded. Max is {np.iinfo(np.uint16).max}, got {time_steps}")
-
+        # create a grid of all possible (isi, rate) pairs
+        value_grid = np.meshgrid(
+            np.arange(self.min_isi, self.max_isi + 1),
+            np.arange(self.min_rate, self.max_rate + 1)
+        )
+        # create class assignment matrix (half of the values are class 0, half class 1)
+        class_assignment = self._class_assign_matrix(
+            np.zeros_like(
+                value_grid[0],
+                dtype = np.uint8
+            )
+        )
+        self.isis    = value_grid[0].flatten()
+        self.rates   = value_grid[1].flatten()
+        self.classes = class_assignment.flatten()
 
         self.rng = np.random.default_rng()
 
-        self.time_steps = time_steps
-        self.neurons = neurons
-        self.inter_spike_intervals = inter_spike_intervals
-        self.no_spike_pairs_per_neuron = int(rate * (self.time_steps / 1000))
-        self.total_samples = int(no_samples)
-
-
-    def gen_sample(
-        self,
-        isi: int = 10,
+    def _class_assign_matrix(
+            self, 
+            arr: np.ndarray
     ) -> np.ndarray:
-        """
-        generate one sample 
-        """
-        if isi < 1 or isi > 10:
-            raise ValueError(f"ISI is in an invalid range. Valid range is [1;10]. Actual value: {isi}")
-        
-        full_data = []
-        for i in range(self.neurons):
-            valid_starts = np.arange(
-                self.time_steps - isi, 
-                dtype = np.uint16, 
-            )
+        slope = arr.shape[0]/arr.shape[1]
 
-            cur_no = 0
-            while cur_no < self.no_spike_pairs_per_neuron:
-                sample = self.rng.choice(
-                    valid_starts,
-                    size = (1,),
-                    replace = True,
-                    shuffle = False # not needed, but makes it faster
-                )[0]
-                full_data.extend(((sample, i), (sample + isi, i)))
-                valid_starts = valid_starts[abs(valid_starts - sample) > isi]
-                cur_no += 1
+        # fill the "upper triangle" from [0,0] to [n_rows, n_cols]
+        for i in range(arr.shape[0]):
+            for j in range(arr.shape[1]):
+                # everything that is above or on the slope is 1
+                if j >= i * slope:
+                    arr[i, j] = 1
+        return arr
+    
+    def _generate_sample(
+        self,
+        isi: int,
+        rate: int
+    ) -> np.ndarray:
         
-        spikes = np.array(full_data)
+        # calculate number of spikepairs, given in [source]
+        spk_pairs = int(rate * (self.time_steps / 1000))
+
+        # preallocate the resulting array
         out = np.zeros(
             shape = (self.time_steps, self.neurons),
             dtype = np.float32,
         )
-        out[spikes[:,0], spikes[:,1]] += 1
+
+        for i in range(self.neurons):
+            mask = np.ones(
+                (self.time_steps - isi,), 
+                dtype = bool
+            )
+
+            cur_no = 0
+            while cur_no < spk_pairs:
+                sample = self.rng.choice(
+                    np.flatnonzero(mask),
+                    size = (1,),
+                    replace = True,
+                    shuffle = False # not needed, but makes it faster
+                )[0]
+
+                # set value directly in the array
+                out[sample, i] += 1
+                out[sample + isi, i] += 1
+                
+                # check boundaries and handle all cases
+                left = max(sample - isi, 0)
+                right = min(sample + isi + 1, len(mask))
+                mask[left:right] = False
+                cur_no += 1
+                
+                # break if no valid positions are left
+                if mask.sum() == 0:
+                    break
+
         return out
     
+    # TODO: does not work, optimize
+    def _jitter(
+        self,
+        sample: np.ndarray,
+    ) -> np.ndarray:
+        
+        spikes = np.nonzero(sample)
+        valid_to = np.nonzero(sample == 0)
+
+        for neuron in range(sample.shape[1]):
+            mask = spikes[1] == neuron
+            to_move = math.ceil(len(spikes[0][mask]) * self.jitter)
+            selected_spikes = self.rng.choice(
+                spikes[0][mask],
+                size = to_move,
+                replace = False,
+                shuffle = False
+            )
+            new_positions = self.rng.choice(
+                valid_to[0][mask],
+                size = to_move,
+                replace = False,
+                shuffle = False
+            )
+            sample[selected_spikes, neuron] -= 1
+            sample[new_positions, neuron] += 1
+        
+        return sample
+
+
+    def generate_samples(
+        self,
+        no_samples: int = 3000,
+    ) -> tuple[np.ndarray, np.ndarray]:
+        samples_per_class = math.ceil(no_samples / 2)
+        samples = []
+        labels  = []
+
+        for i in range(2):
+            indices = self.rng.choice(
+                np.nonzero(self.classes == i)[0],
+                size = samples_per_class,
+                replace = True
+            )
+            for idx in indices:
+                # generate sample with self.isis[idx] and self.rates[idx]
+                sample = self._generate_sample(
+                    isi = self.isis[idx],
+                    rate = self.rates[idx]
+                )
+                if self.jitter > 0:
+                    sample = self._jitter(
+                        sample = sample,
+                    )
+                # append to list of samples
+                # append class label to list of labels
+                samples.append(sample)
+                labels.append(i)
+        samples = np.stack(samples)
+        labels = np.array(labels)
+        return samples, labels
+
     def generate_dataset(
         self
     ) -> torch.utils.data.DataLoader:
-        samples_per_class = int(self.total_samples / len(self.inter_spike_intervals))
-        missing = self.total_samples - samples_per_class * len(self.inter_spike_intervals) 
+        pass
+    
 
-        samples = [samples_per_class] * len(self.inter_spike_intervals)
-
-        if missing != 0:
-            samples[:missing] += 1
-        
-        data = []
-        target = []
-        for i, no in enumerate(samples):
-            data.extend([
-                self.gen_sample(
-                    isi = self.inter_spike_intervals[i]
-                ) for _ in range(no)
-            ])
-            
-            target.append(np.full(
-                shape = (no,),
-                fill_value = i,
-                dtype = np.float32
-            ))
-
-        data = np.stack(data)
-        target = np.concatenate(target)
-
-        data = torch.from_numpy(data)
-        target = torch.from_numpy(target)
-
-        dataset = torch.utils.data.TensorDataset(data, target)
-        dataloader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size = 64,
-            shuffle = True,
-            num_workers = 6
-        )
-        return dataloader
-
-# def unison_shuffled_copies(
-#         arr_0: np.ndarray, 
-#         arr_1: np.ndarray
-#     ) -> tuple[np.ndarray, np.ndarray]:
-#     """
-#         Convenience function to shuffle two arrays in unison along their first dimension.
-#         :param arr_0: First array to shuffle.
-#         :type arr_0: np.ndarray, required
-#         :param arr_1: Second array to shuffle.
-#         :type arr_1: np.ndarray, required
-#         :return: Tuple of shuffled arrays.
-#         :rtype: tuple[np.ndarray, np.ndarray]
-#     """
-#     # set random seed for reproducibility
-#     np.random.seed(42)
-#     assert len(arr_0) == len(arr_1)
-
-#     p = np.random.permutation(len(arr_0))
-#     return arr_0[p], arr_1[p]
 
 
 
 if __name__ == "__main__":
 
-    data = DataGenerator(
-        time_steps = 1000,
-        inter_spike_intervals = np.arange(1,11)
+    data = NewGenerator(
+        time_steps = 100,
+        jitter = 0.1,
+        min_isi = 1,
+        max_isi = 10,
+        min_rate = 5,
+        max_rate = 20,
     )
-    # data.generate_dataset()
+    data.generate_samples()
 
+    # TODO: benchmark
     # print(timeit.timeit(
-    #     data.gen_data,
+    #     data.generate_samples,
     #     number = 10000
     # ))
-    # # 22.460367957000017
-    # # 22.72771648700018
-    # # 22.611762075000115
-
-    # print(timeit.timeit(
-    #     data.gen_data_torch_cpu,
-    #     number = 10000
-    # ))
-    # # 48.99222566199933
-    # # 48.467646289000186
-
-    # print(timeit.timeit(
-    #     data.gen_data_torch_cuda,
-    #     number = 10000
-    # ))
-    # 157.93103715899997
-
-
-    print(timeit.timeit(
-        data.gen_sample_lst,
-        number = 10000
-    ))
-    # 24.029359733998717
-    # 23.42586822999874
-    print(timeit.timeit(
-        data.gen_sample_np,
-        number = 10000
-    ))
-    # 24.91761143199983
-    # 24.41251021700009
