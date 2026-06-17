e58cc9eec733f1bd9b4065e7a5e26251a1efec3c
MeinChef <126389277+MeinChef@users.noreply.github.com>
2026-06-17 15:28:53 +0200

    added lif-neuron visualisation

--- src/visualisation.py
+++ src/visualisation.py
@@ -1,6 +1,8 @@
 from imports import numpy as np
 from imports import plt
 from imports import torch
+from imports import snntorch as snn
+from imports import spikeplot as splt
 from imports import Figure
 from misc import make_path# , load_spk_rec
 
@@ -94,3 +96,74 @@ def plot_epoch_losses(
     plt.grid(True)
     plt.tight_layout()
     plt.show()
+
+def plot_lif_voltage() -> None:
+    """
+    Adapted from https://snntorch.readthedocs.io/en/latest/tutorials/tutorial_2.html
+    """
+    neuron = snn.Leaky(0.9)
+
+    # Initialize inputs and outputs
+    cur_in = torch.cat((torch.zeros(10, 1), torch.ones(50, 1)*0.15, torch.zeros(20, 1)), 0)
+    mem = torch.zeros(1)
+    spk_out = torch.zeros(1)
+    mem_rec = [mem]
+    spk_rec = [spk_out]
+
+    # Simulation run across 100 time steps.
+    for step in range(cur_in.shape[0]):
+        spk_out, mem = neuron(cur_in[step], mem)
+        mem_rec.append(mem)
+        spk_rec.append(spk_out)
+
+    # convert lists to tensors
+    mem_rec = torch.stack(mem_rec)
+    spk_rec = torch.stack(spk_rec)
+
+
+    # def plot_cur_mem_spk(cur, mem, spk, thr_line=False, vline=False, title=False, ylim_max2=1.25):
+    # Generate Plots
+    fig, ax = plt.subplots(
+        3, 
+        figsize = (8,6), 
+        sharex = True, 
+        gridspec_kw = {'height_ratios': [1, 1, 0.4]}
+    )
+
+    # Plot input current
+    ax[0].plot(cur_in, c = "tab:orange")
+    ax[0].set_ylim([0, 0.4])
+    ax[0].set_xlim([0, cur_in.shape[0]])
+    ax[0].set_ylabel("Input Current ($I_{in}$)")
+    ax[0].set_title("Leaky Integrate-and-Fire Neuron with step input")
+
+    # Plot membrane potential
+    ax[1].plot(mem_rec)
+    ax[1].set_ylim([0, 1.3]) 
+    ax[1].set_ylabel("Membrane Potential ($U_{mem}$)")
+    ax[1].axhline(
+        y = 1, 
+        alpha = 0.25, 
+        linestyle = "dashed", 
+        c = "black", 
+        linewidth = 2
+    )
+    plt.xlabel("Time step")
+
+    # Plot output spike using spikeplot
+    splt.raster(
+        spk_rec, 
+        ax[2], 
+        s = 400, 
+        c = "black", 
+        marker = "|"
+    )
+
+    plt.ylabel("Output spikes")
+    plt.yticks([]) 
+
+    plt.savefig("./img/lif-neuron.png")
+    plt.show()
+
+if __name__ == "__main__":
+    plot_lif_voltage()
